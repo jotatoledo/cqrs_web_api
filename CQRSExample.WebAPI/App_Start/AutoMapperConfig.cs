7e290ba893b6ed2cb8671b363779a9f9bdcde5b2
Toledo,Jose <jotatoledon@gmail.com>
2017-09-16 22:53:01 +0200

    feat(): added AutoMapper config

--- CQRSExample.WebAPI/App_Start/AutoMapperConfig.cs
+++ CQRSExample.WebAPI/App_Start/AutoMapperConfig.cs
@@ -1,8 +1,9 @@
 ﻿using AutoMapper;
+using CQRSExample.Data.Sql.StarterDb;
+using CQRSExample.Model.MaterialNumber;
+using CQRSExample.Model.Plant;
+using CQRSExample.Model.WorkCenter;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CQRSExample.WebAPI.App_Start
 {
@@ -21,6 +22,9 @@ namespace CQRSExample.WebAPI.App_Start
             config.AssertConfigurationIsValid();
             Mapper.Initialize(cfg =>
             {
+                cfg.CreateMap<Plant, PlantDetails>();
+                cfg.CreateMap<WorkCenter, WorkCenterDetails>();
+                cfg.CreateMap<MaterialNumber, MaterialNumberDetails>();
             });
             Mapper.AssertConfigurationIsValid();
             return config;
