e96a92af4d8293bac5b8f78d73e57a08652b0567
Toledo,Jose <jotatoledon@gmail.com>
2017-09-16 21:15:42 +0200

    feature(workshop/material-numbers): added CQRS classes

---
+++ CQRSExample.Domain.WorkCenters/MaterialNumbers/Associate.cs
@@ -0,0 +1,48 @@
+﻿using CQRSExample.Data.Sql.StarterDb;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSExample.Domain.WorkCenters.MaterialNumbers
+{
+    public class Associate
+    {
+        public class Command : IRequest
+        {
+            public string PlantId { get; set; }
+            public string WorkCenterId { get; set; }
+            public IEnumerable<string> MaterialNumberId { get; set; }
+
+            public Command(string plantId, string workCenterId, IEnumerable<string> materialNumberId)
+            {
+                PlantId = plantId;
+                WorkCenterId = workCenterId;
+                MaterialNumberId = materialNumberId;
+            }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly StarterDbEntities _context;
+
+            public CommandHandler(StarterDbEntities context)
+            {
+                if (context == null) throw new ArgumentNullException(nameof(context));
+                _context = context;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var workCenter = await _context.WorkCenter
+                    .SingleOrDefaultAsync(wc => wc.Id == message.WorkCenterId && wc.Plant.Id == message.PlantId);
+                if (workCenter == null) throw new InvalidOperationException();
+                var materialNumbers = _context.MaterialNumber.Where(mn => message.MaterialNumberId.Contains(mn.Id));
+                await materialNumbers.ForEachAsync(mn => workCenter.MaterialNumber.Add(mn));
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ CQRSExample.Domain.WorkCenters/MaterialNumbers/Dissociate.cs
@@ -0,0 +1,51 @@
+﻿using CQRSExample.Data.Sql.StarterDb;
+using MediatR;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSExample.Domain.WorkCenters.MaterialNumbers
+{
+    public class Dissociate
+    {
+        public class Command : IRequest
+        {
+            public string PlantId { get; set; }
+            public string WorkCenterId { get; set; }
+            public string MaterialNumberId { get; set; }
+
+            public Command(string plantId, string workCenterId, string materialNumberId)
+            {
+                PlantId = plantId;
+                WorkCenterId = workCenterId;
+                MaterialNumberId = materialNumberId;
+            }
+        }
+
+        public class CommandHandler : IAsyncRequestHandler<Command>
+        {
+            private readonly StarterDbEntities _context;
+
+            public CommandHandler(StarterDbEntities context)
+            {
+                if (context == null) throw new ArgumentNullException(nameof(context));
+                _context = context;
+            }
+
+            public async Task Handle(Command message)
+            {
+                var workCenter = await _context.WorkCenter
+                    .SingleOrDefaultAsync(wc => wc.Id == message.WorkCenterId && wc.Plant.Id == message.PlantId);
+                if (workCenter == null) throw new InvalidOperationException();
+
+                var materialNumber = workCenter.MaterialNumber
+                    .SingleOrDefault(mn => mn.Id == message.MaterialNumberId);
+                if (materialNumber == null) throw new InvalidOperationException();
+
+                workCenter.MaterialNumber.Remove(materialNumber);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
